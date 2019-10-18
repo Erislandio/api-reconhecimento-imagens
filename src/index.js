f3ba52b60252045f9c82f19431b9d60e3cbd1b9f
Erislandio <erislandio.soares@acct.global>
2019-10-18 20:25:09 -0300

    adiçao do middleware bodyparser

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(CORS())
 
